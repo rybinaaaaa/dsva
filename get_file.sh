f026099bf21ff31a0574075928e2f6f8661996c6
rybinali <rybinaaaaa.a@gmail.com>
2025-01-16 21:59:39 +0100

    add correction for bash script

--- get_file.sh
+++ get_file.sh
@@ -2,5 +2,5 @@
    PORT=$2
    NODE_ID=$3
 
-RESPONSE=$(curl "http://$HOST:$PORT/$NODE_ID/file/get")
+RESPONSE=$(curl "http://$HOST:$PORT/1$NODE_ID/file/get")
   echo "Response from node $NODE_ID: $RESPONSE"
